9775d30011bd94711e11a87f446dcc9166cff353
iechenyb <zzuchenyb@sina.com>
2018-05-28 11:05:48 +0800

    提交更新

---
+++ kiiik-vas/vas-parent/vas-api/src/main/java/com/kiiik/pub/exception/FunctionIdNotFoundException.java
@@ -0,0 +1,14 @@
+package com.kiiik.pub.exception;
+
+/**
+ * 作者 : iechenyb<br>
+ * 类描述: 说点啥<br>
+ * 创建时间: 2018年5月15日
+ */
+public class FunctionIdNotFoundException extends VasException {
+	private static final long serialVersionUID = 1L;
+
+	public FunctionIdNotFoundException(String message) {
+		super(message);
+	}
+}
