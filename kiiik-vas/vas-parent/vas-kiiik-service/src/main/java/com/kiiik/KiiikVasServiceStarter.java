1d9b1317ee39020df4060a1e2240a11100f77f39
iechenyb <zzuchenyb@sina.com>
2018-09-17 09:52:41 +0800

    升级版注册中心

--- kiiik-vas/vas-parent/vas-kiiik-service/src/main/java/com/kiiik/KiiikVasServiceStarter.java
+++ kiiik-vas/vas-parent/vas-kiiik-service/src/main/java/com/kiiik/KiiikVasServiceStarter.java
@@ -3,6 +3,7 @@ package com.kiiik;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -11,6 +12,7 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class KiiikVasServiceStarter extends WebMvcConfigurerAdapter {
 	@Bean
     public HttpMessageConverters fastJsonHttpMessageConverters(){
