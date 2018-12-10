ab9411728f43a097dbce25131312bba294c678bd
iechenyb <zzuchenyb@sina.com>
2018-12-10 08:59:05 +0800

    task工程

---
+++ springboot-task-mannal/src/main/java/com/kiiik/web/system/controller/CronController.java
@@ -0,0 +1,11 @@
+package com.kiiik.web.system.controller;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+/**
+ *作者 : iechenyb<br>
+ *类描述: 说点啥<br>
+ *创建时间: 2018年12月10日
+ */
+public class CronController {
+	Log log = LogFactory.getLog(CronController.class);
+}
